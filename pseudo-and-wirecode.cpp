325d616fd715e49db50e0223c3927f64ba425e98
Jed Aficial's PC <jedaficial1@gmail.com>
2024-11-06 20:21:03 -0800

    Initial Commit

---
+++ pseudo-and-wirecode.cpp
@@ -0,0 +1,9 @@
+// COMSC-210 | Jed Aficial | Lab 29
+// github link: https://github.com/jaficial/210-lab-29-30-31.git
+
+// PSEUDO CODE FOR Krazy Kreatures Collectible Cards
+// NOTE: Changes are to be made, and some features from pseudocode and wirecode may be altered, deleted, or added for the final program
+
+/*
+
+*/
